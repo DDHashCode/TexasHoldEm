1601ca03e1630dc4992a11487851c1472d84c823
DDHashCode <30550195+DDHashCode@users.noreply.github.com>
2018-08-09 11:19:44 +0200

    Initial commit

---
+++ Colour.java
@@ -0,0 +1,4 @@
+public enum Colour {
+    HEART, SPADE, DIAMOND, CLUB
+    //czerwo, wino, dzwon, żołądź
+}
---
+++ TexasHoldEm.java
@@ -0,0 +1,5 @@
+public class TexasHoldEm {
+    public static void main(String[] args) {
+
+    }
+}
